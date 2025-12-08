81b360b8b5a00b0bb5d2c2d60e0e982132647176
mishkatjamal <mishkatjamal13@gmail.com>
2025-12-08 11:51:26 +0530

    call back hell

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let pr = new Promise(function(res,rej){
-  let rn = Math.floor(Math.random()*10);
-  if(rn >5){
-    return res("resolved" + rn);
-  }else{
-    return rej("reject" + rn)
-  }
-})
-
-pr.then(function(val){
-  console.log(val);
-  
-}).catch(function(val){
-  console.log(val);
-  
+function abc(fn){
+  fn(function(fn2){
+    fn2(function(fn4){
+      fn4(function(fn6){
+        fn6();
+      })
+    });
+  });
+}
+abc(function(fn1){
+fn1(function(fn3){
+fn3(function(fn5){
+fn5(function(){
+console.log("fun called");
+});
+});
+});
 })
